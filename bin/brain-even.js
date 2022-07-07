f888df255d9eceabadda390b3bf785a377a2081c
di-iv <67899051+di-iv@users.noreply.github.com>
2022-07-07 11:12:32 +0300

    feat: brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let roundCount = 3;
+
+function play(roundLeft) {
+  if (roundLeft === 0) {
+    console.log('Congratulations, Bill!');
+    return;
+  }
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
+  console.log(`Question: ${randomNumber}`);
+  const isNumberEven = randomNumber % 2 === 0;
+  const correctAnswer = isNumberEven ? 'yes' : 'no';
+  const userAnswer = readlineSync.question('Your answer: ');
+  const isNotCorrectUserAnswer = correctAnswer !== userAnswer;
+  if (isNotCorrectUserAnswer) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log('Let\'s try again, Bill!');
+    return;
+  }
+  console.log('Correct!');
+  play(roundCount -= 1);
+}
+play(roundCount);
